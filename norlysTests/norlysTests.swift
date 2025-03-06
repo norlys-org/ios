9c5b7815c0cb3286a2c6e5e117575df0ab7b64fc
Hugo Lageneste <hugo@testnor.com>
2025-03-06 11:38:33 +0100

    Initial Commit

---
+++ norlysTests/norlysTests.swift
@@ -0,0 +1,17 @@
+//
+//  norlysTests.swift
+//  norlysTests
+//
+//  Created by Hugo Lageneste on 06/03/2025.
+//
+
+import Testing
+@testable import norlys
+
+struct norlysTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
